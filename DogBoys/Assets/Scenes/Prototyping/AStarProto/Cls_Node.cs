6e46fca3be3b0bb0cf3a9661c00b0c559d77ee22
Matthew Quaschnick <mrq27@drexel.edu>
2018-07-10 22:34:16 -0400

    A* grid movement boissss bark bark
    This is step one for prototyping grid movement using A*
    Can be found in Assets/Scenes/Prototyping
    Click play, and click anywhere valid on the grid, the editor will update with the path to the point you clicked on
    ToDo:
    - Move startPosition to targetPosition
    - Limit max move distance

---
+++ DogBoys/Assets/Scenes/Prototyping/AStarProto/Cls_Node.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cls_Node {
+
+	public int gridX;
+	public int gridY;
+	public bool isWall;
+	public Vector3 position;
+	public Cls_Node parent;
+	public int gCost;
+	public int hCost;
+	public int fCost { get { return gCost + hCost; } }
+
+	public Cls_Node (bool a_isWall, Vector3 b_position, int c_gridX, int d_gridY) {
+		isWall = a_isWall;
+		position = b_position;
+		gridX = c_gridX;
+		gridY = d_gridY;
+	}
+
+
+}
---
+++ DogBoys/Assets/Scenes/Prototyping/AStarProto/Scr_Pathfinding.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_Pathfinding : MonoBehaviour {
+
+	private Scr_Grid grid;
+	public Transform startPosition;
+	public Transform targetPosition;
+
+	private void Awake() {
+		grid = GetComponent<Scr_Grid>();
+	}
+
+	private void Update() {
+		FindPath(startPosition.position, targetPosition.position);
+	}
+
+
+	private void FindPath(Vector3 a_startPosition, Vector3 b_targetPosition) {
+		Cls_Node startNode = grid.NodeFromWorldPosition(a_startPosition);
+		Cls_Node targetNode = grid.NodeFromWorldPosition(b_targetPosition);
+
+		List<Cls_Node> OpenList = new List<Cls_Node>();
+		List<Cls_Node> ClosedList = new List<Cls_Node>(); // Tutorial uses HashSet since you don't need to access the closed list anymore for A* algorithm, I'm going to use a List to avoid confusion
+	
+		OpenList.Add(startNode);
+
+		while (OpenList.Count > 0) {
+			Cls_Node currentNode = OpenList[0];
+
+			for (int i = 0; i < OpenList.Count; i++) {
+				if ((OpenList[i].fCost < currentNode.fCost || OpenList[i].fCost == currentNode.fCost) && OpenList[i].hCost < currentNode.hCost) {
+					currentNode = OpenList[i];
+				}
+			}
+
+			OpenList.Remove(currentNode);
+			ClosedList.Add(currentNode);
+
+			if(currentNode == targetNode) {
+				GetFinalPath(startNode, targetNode);
+			}
+
+			foreach (Cls_Node neighborNode in grid.GetNeighboringNodes(currentNode)) {
+				if(neighborNode.isWall || ClosedList.Contains(neighborNode)) {
+					continue; // Skip if wall or in closed list
+				}
+
+				int moveCost = currentNode.gCost + GetManhattanDistance(currentNode, neighborNode);
+
+				if (moveCost < neighborNode.gCost || !OpenList.Contains(neighborNode)) {
+					neighborNode.gCost = moveCost;
+					neighborNode.hCost = GetManhattanDistance(neighborNode, targetNode);
+					neighborNode.parent = currentNode;
+
+					if(!OpenList.Contains(neighborNode)) {
+						OpenList.Add(neighborNode);
+					}
+				}
+			}
+		}
+	}
+
+	private int GetManhattanDistance(Cls_Node nodeA, Cls_Node nodeB) { // Distance between 2 nodes, aparantly named after calculating distances of square blocks in Manhattan, NY
+		int iX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+		int iY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+
+		return iX + iY;
+	}
+
+
+	private void GetFinalPath(Cls_Node a_startNode, Cls_Node b_targetNode) {
+		List<Cls_Node> finalPath = new List<Cls_Node>();
+		Cls_Node currentNode = b_targetNode;
+
+		while (currentNode != a_startNode) {
+			finalPath.Add(currentNode);
+			currentNode = currentNode.parent;
+		}
+
+		finalPath.Reverse();
+
+		grid.finalPath = finalPath;
+	}
+}
