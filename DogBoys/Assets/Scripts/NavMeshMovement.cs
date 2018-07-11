5d97afe575c3500266f1a0876f780c6263e93745
Mason Griff <masonalexgriff@gmail.com>
2018-07-11 08:36:54 -0400

    Working NavMesh Movement
    Working movement and walking to location via nav mesh and nav agent. Sample scene available to see how they work. To be merged with Matt's movement script at his discretion.

--- DogBoys/Assets/Scripts/NavMeshMovement.cs
+++ DogBoys/Assets/Scripts/NavMeshMovement.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class NavMeshMovement : MonoBehaviour
-{ // code to be combined with Matt's movement script
-    public Transform targetPos, currentPos, nextPos;
+{ // code to be combined with Matt's movement script and probably both combined into Character script.
+
+    /*
+     Note:
+         The following is for when starting a new scene...
+         Be sure to set a plane just at level with the top of the ground to where it's not visible but is is reachable as a NavMesh.
+         Open the Navigation window and go to object tab
+         Select the plane and hit Navigation Static in the Navigation Window and make sure the dropdown is set to Walkable
+         Select the buildings and obstacles in the scene next, hit static like before but make sure these are set to Not Walkable
+         Now go to Bake tab and bake it but make sure Agent Radius in bake is set to 0.1
+         
+     Note:
+         This script doesn't take turns or anything into account, 
+         as it will be merged with Matt's script and added to the Character controller,
+         this will hopefully not be a problem as I imagine the code will be edited when they're combined.
+         */
+    public Transform targetPos, currentPos;
 
     public bool AtDestination;
     public UnityEngine.AI.NavMeshAgent myAgent;
@@ -14,31 +29,47 @@ public class NavMeshMovement : MonoBehaviour
     public bool NavIsOnGround;
     float onMeshThreshold = 3;
 
+    [Tooltip("Set this active to disable relying on tiles. Only used for testing the movement system, turn off in actual project.")]
+    public bool TestingScript;
+
     private void Start()
     {
         myAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
         //IsAgentOnNavMesh(this.gameObject);
         NavIsOnGround = myAgent.isOnNavMesh; //Checks if NavMesh Agent is on ground. If this bool is false, the agent won't move and there will be tons of "Set Destination" errors
         AtDestination = true;
+        myAnim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        NavIsOnGround = myAgent.isOnNavMesh;
-        if (currentPos != targetPos)
+        NavIsOnGround = myAgent.isOnNavMesh; //Checks if NavMesh Agent is on ground. If this bool is false, the agent won't move and there will be tons of "Set Destination" errors
+        if (!TestingScript)
         {
-            AtDestination = false;
+            if (Camera_mouse.instance.GoalTile != null) //If the goal tile isn't null, sets the target position to move to that position
+            { targetPos = Camera_mouse.instance.GoalTile; }
         }
-        //targetPos = Camera_mouse.instance.GoalTile;
-        if (!AtDestination)
+        if (NavIsOnGround)
         {
-            myAgent.destination = targetPos.position;
-            currentPos = targetPos;
-        }
-        if (myAgent.transform.position.x == targetPos.position.x && myAgent.transform.position.z == targetPos.position.z)
-        {
-            AtDestination = true;
-            currentPos = targetPos;
+            if (currentPos != targetPos)
+            {
+                AtDestination = false;
+            }
+            //targetPos = Camera_mouse.instance.GoalTile;
+            if (!AtDestination)
+            {
+                myAgent.isStopped = false;
+                myAgent.destination = targetPos.position;
+                myAnim.SetBool("a_isRunning", true);
+                //currentPos = targetPos;
+            }
+            if (myAgent.transform.position.x == targetPos.position.x && myAgent.transform.position.z == targetPos.position.z)
+            {
+                AtDestination = true;
+                myAgent.isStopped = true;
+                myAnim.SetBool("a_isRunning", false);
+                currentPos = targetPos;
+            }
         }
     }
 
