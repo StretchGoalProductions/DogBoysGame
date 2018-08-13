7738ebcbc206d2fc647ead001f428a0c2f204df7
Matthew Quaschnick <mrq27@drexel.edu>
2018-08-13 18:19:30 -0400

    Fixed movement bug, updated team turn display

--- DogBoys/Assets/Scripts/UI/Scr_TurnText.cs
+++ DogBoys/Assets/Scripts/UI/Scr_TurnText.cs
@@ -8,6 +8,15 @@ public class Scr_TurnText : MonoBehaviour {
     string CurrentTeam;
 
     public GameObject RedTeam, BlueTeam;
+    public GameObject redActions, blueActions;
+
+    void Start() {
+        Debug.Log(Scr_TeamController.menuBlueDogs.Count);
+        for (int i=0; i < Scr_TeamController.menuBlueDogs.Count; i++) {
+            redActions.transform.GetChild(i).gameObject.SetActive(true);
+            blueActions.transform.GetChild(i).gameObject.SetActive(true);
+        }
+    }
 
 	void Update () {
 		CurrentTeam = Scr_GameController.displayTeamName_;
@@ -15,11 +24,19 @@ public class Scr_TurnText : MonoBehaviour {
         {
             RedTeam.SetActive(false);
             BlueTeam.SetActive(true);
+
+            for (int i=0; i < Scr_TeamController.menuBlueDogs.Count; i++) {
+                blueActions.transform.GetChild(i).GetChild(0).GetComponent<Text>().text = Scr_TeamController.blueTeam[i].GetComponent<Scr_DogBase>().movesLeft.ToString();
+            }
         }
         else if (CurrentTeam == "Red Rovers")
         {
             RedTeam.SetActive(true);
             BlueTeam.SetActive(false);
+
+            for (int i=0; i < Scr_TeamController.menuRedDogs.Count; i++) {
+                redActions.transform.GetChild(i).GetChild(0).GetComponent<Text>().text = Scr_TeamController.redTeam[i].GetComponent<Scr_DogBase>().movesLeft.ToString();
+            }
         }
     }
 }
